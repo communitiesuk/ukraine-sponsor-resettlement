fc7c6341ade4250c484d8ddebf4c0f2e98b0a287
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-10 15:06:41 +0000

    Add registration table with minimal properties

--- db/migrate/20220310140825_create_registration_table.rb
+++ db/migrate/20220310140825_create_registration_table.rb
@@ -1,10 +1,9 @@
 class CreateRegistrationTable < ActiveRecord::Migration[7.0]
   def change
     create_table :registrations do |t|
-      t.integer :Id
-      t.uuid :Reference
-      t.boolean: IsTransferred
-      
+      t.uuid :reference
+      t.boolean :is_transferred
+
       t.timestamps
     end
   end
