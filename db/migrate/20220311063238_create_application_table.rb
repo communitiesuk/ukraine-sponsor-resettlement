15894f8e9d8579109a576526cf0a3e08b23b27dc
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-11 09:12:24 +0000

    Fix Linting issue with single quotes

--- db/migrate/20220311063238_create_application_table.rb
+++ db/migrate/20220311063238_create_application_table.rb
@@ -1,6 +1,6 @@
 class CreateApplicationTable < ActiveRecord::Migration[7.0]
   def change
-    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+    enable_extension "pgcrypto" unless extension_enabled?("pgcrypto")
 
     create_table :applications, id: :uuid do |t|
       t.string :reference
