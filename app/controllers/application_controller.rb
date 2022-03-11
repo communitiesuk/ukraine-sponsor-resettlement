fb3f30816ddc6de28524c4922a633e63e64c693d
Ben Pirt <ben@pirt.co.uk>
2022-03-11 15:05:58 +0000

    Add basic auth in non-dev environments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   default_form_builder GOVUKDesignSystemFormBuilder::FormBuilder
+
+  unless Rails.env.development?
+    http_basic_authenticate_with name: ENV.fetch("BASIC_AUTH_USER"), password: ENV.fetch("BASIC_AUTH_PASS")
+  end
 end
