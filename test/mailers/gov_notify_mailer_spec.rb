592fb56949cbb5bbc2dad05ca3845472e424a7ed
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-11 10:53:33 +0000

    Add RSpec for Gov Notify mailer

---
+++ test/mailers/gov_notify_mailer_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe "Gov Notify Mailer" do
+  context "when sponsor has completed their application form" do
+    let(:notify_client) { instance_double(Notifications::Client) }
+  end
+end
