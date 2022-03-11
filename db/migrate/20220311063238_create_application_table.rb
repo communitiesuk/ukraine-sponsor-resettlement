97f2fb1320d18988893d04834c8c699d507fa08b
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-11 10:34:34 +0000

    Fix Linting issues

--- db/migrate/20220311063238_create_application_table.rb
+++ db/migrate/20220311063238_create_application_table.rb
@@ -4,10 +4,10 @@ class CreateApplicationTable < ActiveRecord::Migration[7.0]
 
     create_table :applications, id: :uuid do |t|
       t.string :reference
-      t.string :fullname, :limit => 128, :null => true
-      t.string :email, :limit => 128, :null => true
-      t.json :answers, :null => true
-      t.datetime :transferred_at, :null => true
+      t.string :fullname, limit: 128, null: true
+      t.string :email, limit: 128, null: true
+      t.json :answers, null: true
+      t.datetime :transferred_at, null: true
       t.timestamps
     end
   end
