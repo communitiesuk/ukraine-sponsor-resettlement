b29494ffcbe0e1bc4cdbbcdb111d64cd45df221c
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-11 06:52:29 +0000

    Fix spelling mistake in class name

--- db/migrate/20220311063238_create_appliction_table.rb
+++ db/migrate/20220311063238_create_appliction_table.rb
@@ -1,4 +1,4 @@
-class CreateApplictionTable < ActiveRecord::Migration[7.0]
+class CreateApplicationTable < ActiveRecord::Migration[7.0]
   def change
     create_table :applications do |t|
       t.string :reference
