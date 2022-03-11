2361fb41eaad3dd8f2cd8c840e5a148f24ec7859
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-11 06:34:29 +0000

    Add fields for Application table

--- db/migrate/20220311063238_create_appliction_table.rb
+++ db/migrate/20220311063238_create_appliction_table.rb
@@ -1,6 +1,9 @@
 class CreateApplictionTable < ActiveRecord::Migration[7.0]
   def change
-    create_table :applictions do |t|
+    create_table :applications do |t|
+      t.string :reference
+      t.integer :version
+      t.datetime :transferred_at
 
       t.timestamps
     end
