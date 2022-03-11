9b08c4b12ced490602db98001ebf4009113b1ce8
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-11 10:22:53 +0000

    Add schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_03_11_063238) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "applications", force: :cascade do |t|
+  create_table "applications", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "reference"
-    t.integer "version"
+    t.string "fullname", limit: 128
+    t.string "email", limit: 128
+    t.json "answers"
     t.datetime "transferred_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
