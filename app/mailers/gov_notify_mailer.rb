34ac1311fbd32922c5df165afde4ecb639502bc0
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-14 10:23:10 +0000

    Move API key to test

--- app/mailers/gov_notify_mailer.rb
+++ app/mailers/gov_notify_mailer.rb
@@ -2,7 +2,7 @@ require "notifications/client"
 
 class GovNotifyMailer
   def self.notify_client
-    Notifications::Client.new("ukrainianrefugeesponsorshipschemetest-e8fd0b83-9213-4a38-8632-7a461ad7f71e-1a682262-b4cd-4019-9f2c-4d2ed92453a1")
+    Notifications::Client.new(ENV["GOVUK_NOTIFY_API_KEY"])
   end
 
   def send_email(email, template_id, personalisation = {})
--- test/mailers/gov_notify_mailer_spec.rb
+++ test/mailers/gov_notify_mailer_spec.rb
@@ -1,5 +1,6 @@
 require "./app/mailers/gov_notify_mailer"
 
+TEST_API_KEY = "ukrainianrefugeesponsorshipschemetest-e8fd0b83-9213-4a38-8632-7a461ad7f71e-1a682262-b4cd-4019-9f2c-4d2ed92453a1"
 TEMPLATE_ID = "6c80930d-e25a-4dc6-8383-bb83a2c18d19"
 
 describe "Gov Notify Mailer" do
