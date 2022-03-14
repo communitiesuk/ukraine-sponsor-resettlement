4d4629c66db680e75c721a99d3c1315422ed6ad6
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-14 09:01:23 +0000

    Ensure parameters are being set

--- app/mailers/gov_notify_mailer.rb
+++ app/mailers/gov_notify_mailer.rb
@@ -1,5 +1,14 @@
 require "notifications/client"
 
 class GovNotifyMailer
+  def self.notify_client
+    Notifications::Client.new("ukrainianrefugeesponsorshipschemetest-e8fd0b83-9213-4a38-8632-7a461ad7f71e-1a682262-b4cd-4019-9f2c-4d2ed92453a1")
+  end
 
+  def send_email(email, template_id)
+    notify_client.send_email(
+        email_address: email,
+        template_id: template_id
+        )
+  end
 end
--- test/mailers/gov_notify_mailer_spec.rb
+++ test/mailers/gov_notify_mailer_spec.rb
@@ -1,11 +1,15 @@
 require "./app/mailers/gov_notify_mailer"
 
+TEMPLATE_ID = "6c80930d-e25a-4dc6-8383-bb83a2c18d19"
+
 describe "Gov Notify Mailer" do
   context "when sponsor has completed their application form" do
     it "sends application complete email" do
-      expect(GovNotifyMailer).to receive(:send_email)
+      expect(GovNotifyMailer).to receive(:send_email).with(
+          "no-one@test.com", "6c80930d-e25a-4dc6-8383-bb83a2c18d19"
+          )
 
-      GovNotifyMailer.send_email()
+      GovNotifyMailer.send_email("no-one@test.com", TEMPLATE_ID)
     end
   end
 end
