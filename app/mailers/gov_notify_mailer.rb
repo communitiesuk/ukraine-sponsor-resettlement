b8c172b8cabccc637582a20467187412e9579e11
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-14 09:27:15 +0000

    Add personalisation to send notification client

--- app/mailers/gov_notify_mailer.rb
+++ app/mailers/gov_notify_mailer.rb
@@ -5,10 +5,11 @@ class GovNotifyMailer
     Notifications::Client.new("ukrainianrefugeesponsorshipschemetest-e8fd0b83-9213-4a38-8632-7a461ad7f71e-1a682262-b4cd-4019-9f2c-4d2ed92453a1")
   end
 
-  def send_email(email, template_id)
+  def send_email(email, template_id, personalisation = {})
     notify_client.send_email(
         email_address: email,
-        template_id: template_id
+        template_id: template_id,
+        personalisation: personalisation,
         )
   end
 end
--- test/mailers/gov_notify_mailer_spec.rb
+++ test/mailers/gov_notify_mailer_spec.rb
@@ -11,5 +11,18 @@ describe "Gov Notify Mailer" do
 
       GovNotifyMailer.send_email("no-one@test.com", TEMPLATE_ID)
     end
+
+    it "sends application complete email with personalisation" do
+      personalisation = { fullname: "Bob Jones", reference: "the-reference" }
+
+      expect(GovNotifyMailer).to receive(:send_email).with(
+          "no-one@test.com", "6c80930d-e25a-4dc6-8383-bb83a2c18d19", {
+              fullname: "Bob Jones",
+              reference: "the-reference"
+          }
+      )
+
+      GovNotifyMailer.send_email("no-one@test.com", TEMPLATE_ID, personalisation)
+    end
   end
 end
