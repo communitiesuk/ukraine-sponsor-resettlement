fa0dff79a779067e0f8829e344b62e1c6809a924
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-14 19:25:38 +0000

    Add method for sending organisation confirmation email

--- app/mailers/gov_notify_mailer.rb
+++ app/mailers/gov_notify_mailer.rb
@@ -3,8 +3,17 @@ class GovNotifyMailer < GovukNotifyRails::Mailer
     set_template(ENV["INDIVIDUAL_CONFIRMATION_TEMPLATE_ID"])
 
     set_personalisation(
-        fullname: application.fullname,
-        reference: application.reference
+        fullname: application.fullname
+    )
+
+    mail(to: application.email)
+  end
+
+  def send_organisation_confirmation_email(application)
+    set_template(ENV["ORGANISATION_CONFIRMATION_TEMPLATE_ID"])
+
+    set_personalisation(
+        fullname: application.fullname
     )
 
     mail(to: application.email)
