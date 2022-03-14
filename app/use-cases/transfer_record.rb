f18df05f25b2127216a6df291b1f09e41c62ae60
Ben Pirt <ben@pirt.co.uk>
2022-03-14 12:35:39 +0000

    Make the data transfer job send an auth token

--- app/use-cases/transfer_record.rb
+++ app/use-cases/transfer_record.rb
@@ -10,7 +10,8 @@ class TransferRecord
       application.save!
 
       uri = URI(ENV["REMOTE_API_URL"])
-      res = Net::HTTP.post(uri, application.answers.to_json, "Content-Type" => "application/json")
+      token = ENV["REMOTE_API_TOKEN"]
+      res = Net::HTTP.post(uri, application.answers.to_json, "Content-Type" => "application/json", "Authorization" => "Bearer #{token}")
       unless res.code.to_i >= 200 && res.code.to_i < 300
         raise ActiveRecord::Rollback
       end
