6cb6fffadc020820ca5efe6f989c2ef5b4b431e7
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-14 14:29:12 +0000

    Amend to use application properties

--- app/mailers/gov_notify_mailer.rb
+++ app/mailers/gov_notify_mailer.rb
@@ -2,13 +2,11 @@ class GovNotifyMailer < GovukNotifyRails::Mailer
   def send_individual_confirmation_email(application)
     set_template(ENV["INDIVIDUAL_CONFIRMATION_TEMPLATE_ID"])
 
-    #TODO: use name
     set_personalisation(
-        fullname: "Bob Jones",
+        fullname: application.fullname,
         reference: application.reference
     )
 
-    #TODO: use email address
-    mail(to: "grant.mckenna@madetech.com")
+    mail(to: application.email)
   end
 end
