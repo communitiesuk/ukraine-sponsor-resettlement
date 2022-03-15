4c83fc31fc30fd2719da361f95c3f9b408d0ceed
JohnFarrellDev <johnfarrell@fastmail.com>
2022-03-15 01:47:48 +0000

    update individual tests

--- spec/models/individual_expression_of_interest_spec.rb
+++ spec/models/individual_expression_of_interest_spec.rb
@@ -3,22 +3,15 @@ require "rails_helper"
 RSpec.describe IndividualExpressionOfInterest, type: :model do
   describe "deserialising json into attributes" do
     it "sets attributes based on the json column on load" do
-      answers = { sponsor_type: "family_member", family_or_single_type: "family", living_space_type: "rooms_in_home" }
+      answers = { family_or_single_type: "single_adult", living_space_type: "rooms_in_home_shared_facilities" }
       id = ActiveRecord::Base.connection.insert("INSERT INTO individual_expressions_of_interest (answers, created_at, updated_at) VALUES ('#{JSON.generate(answers)}', NOW(), NOW())")
       record = described_class.find(id)
-      expect(record.sponsor_type).to eq(answers[:sponsor_type])
       expect(record.family_or_single_type).to eq(answers[:family_or_single_type])
       expect(record.living_space_type).to eq(answers[:living_space_type])
     end
   end
 
   describe "validations" do
-    it "validates that the sponsor type matches the allowed values if set" do
-      app = described_class.new
-      app.sponsor_type = "invalid"
-      expect(app.valid?).to be(false)
-      expect(app.errors[:sponsor_type]).to include("Please choose one of the options")
-    end
 
     it "validates that the family or single type matches the allowed values if set" do
       app = described_class.new
@@ -37,13 +30,13 @@ RSpec.describe IndividualExpressionOfInterest, type: :model do
 
   describe "#as_json" do
     it "includes all of the answer values" do
-      app = described_class.new(sponsor_type: :family_member, family_or_single_type: :family, living_space_type: :rooms_in_home)
-      expect(app.as_json).to eq({ sponsor_type: :family_member, family_or_single_type: :family, living_space_type: :rooms_in_home })
+      app = described_class.new(family_or_single_type: :single_adult, living_space_type: :rooms_in_home_shared_facilities)
+      expect(app.as_json).to eq({ family_or_single_type: :single_adult, living_space_type: :rooms_in_home_shared_facilities })
     end
 
     it "does not include empty values" do
-      app = described_class.new(sponsor_type: :family_member)
-      expect(app.as_json).to eq({ sponsor_type: :family_member })
+      app = described_class.new(family_or_single_type: :single_adult)
+      expect(app.as_json).to eq({ family_or_single_type: :single_adult })
     end
   end
 end
--- spec/system/individual_eoi_spec.rb
+++ spec/system/individual_eoi_spec.rb
@@ -11,20 +11,16 @@ RSpec.describe "Individual expression of interest", type: :system do
       expect(page).to have_content("Homes for Ukraine")
       click_link("Register your interest as an individual")
 
-      expect(page).to have_content("Who would you like to sponsor?")
-      choose("Friend or Colleague")
-      click_button("Continue")
-
-      expect(page).to have_content("Are you willing to sponsor a family or a single person?")
-      choose("Single person")
+      expect(page).to have_content("Who can you accommodate?")
+      choose("Single adult")
       click_button("Continue")
 
       expect(page).to have_content("What type of living space can you offer?")
-      choose("Rooms in your home")
+      choose("Rooms in your home with access to shared facilities")
       click_button("Continue")
 
       expect(page).to have_content("Is the entire property accessible to people with mobility impairments?")
-      choose("Yes")
+      choose("Yes, all")
       click_button("Continue")
 
       fill_in("How many single rooms do you have available?", with: 3)
@@ -33,19 +29,11 @@ RSpec.describe "Individual expression of interest", type: :system do
       fill_in("How many double rooms do you have available?", with: 2)
       click_button("Continue")
 
-      fill_in("Enter the postcode of the main property you’re offering.", with: "SG13 7DF")
+      fill_in("Enter the postcode of the main property you’re offering (If you are offering multiple properties, please enter the first part of all the postcodes separated by a comma)", with: "SG13 7DF")
       click_button("Continue")
 
       expect(page).to have_content("How long can you offer accommodation?")
-      choose("3 to 6 months")
-      click_button("Continue")
-
-      expect(page).to have_content("Do you have a current Disclosure and Barring Service (DBS) certificate?")
-      choose("Yes")
-      click_button("Continue")
-
-      expect(page).to have_content("Are you willing to answer more questions to confirm your suitability as a sponsor?")
-      choose("Yes")
+      choose("From 6 to 9 months")
       click_button("Continue")
 
       fill_in("Enter your full name", with: "John Smith")
@@ -54,41 +42,46 @@ RSpec.describe "Individual expression of interest", type: :system do
       fill_in("Enter your email address", with: "john.smith@example.com")
       click_button("Continue")
 
-      fill_in("Enter your mobile number", with: "0123456789")
+      fill_in("Enter your contact telephone number", with: "0123456789")
       click_button("Continue")
 
-      expect(page).to have_content("Sponsor type Friend or Colleague")
-      expect(page).to have_content("Family or individual Single")
-      expect(page).to have_content("Living space type Rooms In Home")
-      expect(page).to have_content("Mobility impaired accessible property Yes")
+      page.check("individual-expression-of-interest-agree-future-contact-true-field")
+      click_button("Continue")
+
+      page.check("individual-expression-of-interest-agree-privacy-statement-true-field")
+      click_button("Continue")
+
+      expect(page).to have_content("Who can you accommodate? Single Adult")
+      expect(page).to have_content("Living space Rooms In Home Shared Facilities")
+      expect(page).to have_content("Mobility impaired accessible property Yes, all")
       expect(page).to have_content("Single rooms available 3")
       expect(page).to have_content("Double rooms available 2")
-      expect(page).to have_content("Property postcode SG13 7DF")
-      expect(page).to have_content("Accomodation length 3 to 6 months")
-      expect(page).to have_content("Disclosure and Barring Service certificate Yes")
-      expect(page).to have_content("Willing to answer more questions Yes")
+      expect(page).to have_content("Property postcode(s) SG13 7DF")
+      expect(page).to have_content("Accomodation length From 6 to 9 months")
       expect(page).to have_content("Name John Smith")
       expect(page).to have_content("Email john.smith@example.com")
-      expect(page).to have_content("Mobile number 0123456789")
+      expect(page).to have_content("Telephone number 0123456789")
+      expect(page).to have_content("Future contact Agreed")
+      expect(page).to have_content("Privacy statement Agreed")
+      
       click_button("Accept And Send")
 
       expect(page).to have_content("Application complete")
 
       application = IndividualExpressionOfInterest.order("created_at DESC").last
       expect(application.as_json).to include({
-        sponsor_type: "friend_or_colleague",
-        family_or_single_type: "single",
-        living_space_type: "rooms_in_home",
-        mobility_impairments_type: "yes",
-        single_room_count: "3",
+        accommodation_length_type: "from_6_to_9_months",
+        agree_future_contact: "true",
+        agree_privacy_statement: "true",  
         double_room_count: "2",
-        postcode: "SG13 7DF",
-        accommodation_length_type: "less_than_6_months",
-        dbs_certificate_type: "yes",
-        answer_more_questions_type: "yes",
-        fullname: "John Smith",
         email: "john.smith@example.com",
-        mobile_number: "0123456789",
+        family_or_single_type: "single_adult",
+        fullname: "John Smith",
+        living_space_type: "rooms_in_home_shared_facilities",
+        mobility_impairments_type: "yes_all",
+        phone_number: "0123456789",
+        postcode: "SG13 7DF",
+        single_room_count: "3",
       })
     end
   end
