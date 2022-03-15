e554af89db2b92cbf573842b1c0281734c7d28fb
Ben Pirt <ben@pirt.co.uk>
2022-03-15 11:14:46 +0000

    Convert object to JSON string before sending

--- app/use-cases/transfer_record.rb
+++ app/use-cases/transfer_record.rb
@@ -11,7 +11,7 @@ class TransferRecord
 
       uri = URI(ENV["REMOTE_API_URL"])
       token = ENV["REMOTE_API_TOKEN"]
-      res = Net::HTTP.post(uri, application.as_json, "Content-Type" => "application/json", "Authorization" => "Bearer #{token}")
+      res = Net::HTTP.post(uri, JSON.generate(application.as_json), "Content-Type" => "application/json", "Authorization" => "Bearer #{token}")
       unless res.code.to_i >= 200 && res.code.to_i < 300
         raise ActiveRecord::Rollback
       end
@@ -26,7 +26,7 @@ class TransferRecord
 
       uri = URI(ENV["REMOTE_API_URL"])
       token = ENV["REMOTE_API_TOKEN"]
-      res = Net::HTTP.post(uri, application.as_json, "Content-Type" => "application/json", "Authorization" => "Bearer #{token}")
+      res = Net::HTTP.post(uri, JSON.generate(application.as_json), "Content-Type" => "application/json", "Authorization" => "Bearer #{token}")
       unless res.code.to_i >= 200 && res.code.to_i < 300
         raise ActiveRecord::Rollback
       end
