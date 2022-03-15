423129c0a792f8aaae7983c1880001674a81003a
Stéphane Meny <smeny@users.noreply.github.com>
2022-03-15 17:05:59 +0000

    Change health endpoint to return 200

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "index#index", as: :root
 
-  get "/health", to: ->(_) { [204, {}, [nil]] }
+  get "/health", to: ->(_) { [200, {}, [nil]] }
 
   get "/individual", to: redirect("/individual/steps/1")
   get "/individual/steps/:stage", to: "individual#display"
