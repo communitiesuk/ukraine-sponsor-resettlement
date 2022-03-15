2dcca9945013405e764503d3d22400bbf6eae585
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-15 09:16:44 +0000

    Amend send update for organisation

--- app/controllers/organisation_controller.rb
+++ app/controllers/organisation_controller.rb
@@ -34,7 +34,7 @@ class OrganisationController < ApplicationController
   def submit
     @application = OrganisationExpressionOfInterest.new(session[:organisation_expression_of_interest])
     @application.save!
-    SendUpdateJob.perform_later(@application.id)
+    SendOrganisationUpdateJob.perform_later(@application.id)
     redirect_to "/organisation/confirm"
   end
 
