aefd3a648246b712eb487b469501d97f36f8c05e
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-15 09:07:52 +0000

    Add method for sending individual update

--- app/jobs/send_individual_update_job.rb
+++ app/jobs/send_individual_update_job.rb
@@ -1,8 +1,8 @@
-class SendUpdateJob < ApplicationJob
+class SendIndividualUpdateJob < ApplicationJob
   queue_as :default
 
   def perform(id)
     Rails.logger.info "Sending update for application #{id}"
-    TransferRecord.execute(id)
+    TransferRecord.execute_individual(id)
   end
 end
