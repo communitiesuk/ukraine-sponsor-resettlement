14ce30dd69712542176ee6c98875646371f2522a
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-03-15 09:15:20 +0000

    Amend log message to be more specific

--- app/jobs/send_individual_update_job.rb
+++ app/jobs/send_individual_update_job.rb
@@ -2,7 +2,7 @@ class SendIndividualUpdateJob < ApplicationJob
   queue_as :default
 
   def perform(id)
-    Rails.logger.info "Sending update for application #{id}"
+    Rails.logger.info "Sending update for individual expression of interest #{id}"
     TransferRecord.execute_individual(id)
   end
 end
