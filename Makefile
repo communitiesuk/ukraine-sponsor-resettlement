75b45e03aad3dfb901bd9500147c2bfb85c847f5
Ben Pirt <ben@pirt.co.uk>
2022-03-16 08:23:38 +0000

    Add more tests for the validations on an individual

--- Makefile
+++ Makefile
@@ -19,3 +19,14 @@ test:
 .PHONY: cf
 cf:
 	docker run -it --mount src=`pwd`,target=/home/piper/app,type=bind ppiper/cf-cli:latest /bin/bash
+
+.PHONY: test-local
+test-local:
+	RAILS_ENV=test \
+	DB_HOST=localhost \
+	DB_USERNAME=ukraine \
+	DB_DATABASE=ukraine_test \
+	DB_PASSWORD=password \
+	VCAP_SERVICES='{"redis":[{"instance_name":"ukraine-sponsor-resettlement-test-redis", "credentials":{"uri":"redis://redis"}}]}' \
+	REMOTE_API_URL=http://mock-api:8081/data \
+	bundle exec rspec
--- spec/models/individual_expression_of_interest_spec.rb
+++ spec/models/individual_expression_of_interest_spec.rb
@@ -12,18 +12,154 @@ RSpec.describe IndividualExpressionOfInterest, type: :model do
   end
 
   describe "validations" do
-    it "validates that the family or single type matches the allowed values if set" do
+    it "Doesn't validate fields if they are not set" do
+      app = described_class.new
+      expect(app.valid?).to be(true)
+    end
+
+    it "validates that the family_type attribute matches the allowed values if set" do
       app = described_class.new
       app.family_type = "invalid"
       expect(app.valid?).to be(false)
       expect(app.errors[:family_type]).to include("Please choose one of the options")
+      app.family_type = "single_adult"
+      expect(app.valid?).to be(true)
     end
 
-    it "validates that the living space type matches the allowed values if set" do
+    it "validates that the living_space attribute matches the allowed values if set" do
       app = described_class.new
       app.living_space = []
       expect(app.valid?).to be(false)
       expect(app.errors[:living_space]).to include("Please choose one or more of the options")
+      app.living_space = %w[rooms_in_home_shared_facilities]
+      expect(app.valid?).to be(true)
+    end
+
+    it "validates that the step_free attribute matches the allowed values if set" do
+      app = described_class.new
+      app.step_free = ""
+      expect(app.valid?).to be(false)
+      expect(app.errors[:step_free]).to include("Please choose one of the options")
+      app.step_free = "yes_all"
+      expect(app.valid?).to be(true)
+    end
+
+    it "validates that the accommodation_length attribute matches the allowed values if set" do
+      app = described_class.new
+      app.accommodation_length = ""
+      expect(app.valid?).to be(false)
+      expect(app.errors[:accommodation_length]).to include("Please choose one of the options")
+      app.accommodation_length = "from_6_to_9_months"
+      expect(app.valid?).to be(true)
+    end
+
+    it "validates that the single_room_count attribute is an integer >= 0" do
+      app = described_class.new
+      app.single_room_count = ""
+      expect(app.valid?).to be(false)
+      expect(app.errors[:single_room_count]).to include("Please enter a non-negative whole number")
+      app.single_room_count = "-1"
+      expect(app.valid?).to be(false)
+      app.single_room_count = "5"
+      expect(app.valid?).to be(true)
+    end
+
+    it "validates that the double_room_count attribute is an integer >= 0" do
+      app = described_class.new
+      app.double_room_count = ""
+      expect(app.valid?).to be(false)
+      expect(app.errors[:double_room_count]).to include("Please enter a non-negative whole number")
+      app.double_room_count = "-1"
+      expect(app.valid?).to be(false)
+      app.double_room_count = "5"
+      expect(app.valid?).to be(true)
+    end
+
+    it "validates that the postcode attribute is at least 2 and less than 100 characters" do
+      app = described_class.new
+      app.postcode = ""
+      expect(app.valid?).to be(false)
+      expect(app.errors[:postcode]).to include("Please enter a valid UK postcode(s)")
+      app.postcode = "A"
+      expect(app.valid?).to be(false)
+      app.postcode = "X" * 101
+      expect(app.valid?).to be(false)
+      app.postcode = "AB"
+      expect(app.valid?).to be(true)
+    end
+
+    it "validates that the phone_number attribute is correct" do
+      app = described_class.new
+      app.phone_number = ""
+      expect(app.valid?).to be(false)
+      expect(app.errors[:phone_number]).to include("Please enter a valid phone number")
+      app.phone_number = "12345678"
+      expect(app.valid?).to be(false)
+      app.phone_number = "(12345678)"
+      expect(app.valid?).to be(false)
+      app.phone_number = "123456789012345"
+      expect(app.valid?).to be(false)
+      app.phone_number = "123456789XXXXXXXXXXXXXXXXXXXXXXX"
+      expect(app.valid?).to be(false)
+      app.phone_number = "(01234) 567890"
+      expect(app.valid?).to be(true)
+    end
+
+    it "validates that the agree_future_contact attribute is correct" do
+      app = described_class.new
+      app.agree_future_contact = ""
+      expect(app.valid?).to be(false)
+      expect(app.errors[:agree_future_contact]).to include("Must be accepted")
+      app.agree_future_contact = "false"
+      expect(app.valid?).to be(false)
+      app.agree_future_contact = "true"
+      expect(app.valid?).to be(true)
+    end
+
+    it "validates that the agree_privacy_statement attribute is correct" do
+      app = described_class.new
+      app.agree_privacy_statement = ""
+      expect(app.valid?).to be(false)
+      expect(app.errors[:agree_privacy_statement]).to include("Must be accepted")
+      app.agree_privacy_statement = "false"
+      expect(app.valid?).to be(false)
+      app.agree_privacy_statement = "true"
+      expect(app.valid?).to be(true)
+    end
+
+    it "validates that the fullname attribute is two words" do
+      app = described_class.new
+      app.fullname = ""
+      expect(app.valid?).to be(false)
+      expect(app.errors[:fullname]).to include("Please enter a valid name")
+      app.fullname = "oneword"
+      expect(app.valid?).to be(false)
+      app.fullname = "first #{'X' * 128}"
+      expect(app.valid?).to be(false)
+      app.fullname = "two words"
+      expect(app.valid?).to be(true)
+    end
+
+    it "validates that the email attribute is correct" do
+      app = described_class.new
+      app.email = ""
+      expect(app.valid?).to be(false)
+      expect(app.errors[:email]).to include("Please enter a valid email address")
+      app.email = "oneword"
+      expect(app.valid?).to be(false)
+      app.email = "#{'x' * 120}@domain.com"
+      expect(app.valid?).to be(false)
+      expect(app.errors[:email]).to include("Please enter a valid email address")
+      app.email = "first@last.com"
+      expect(app.valid?).to be(true)
+    end
+  end
+
+  describe "setting living_space" do
+    it "removes empty elements from the array" do
+      app = described_class.new
+      app.living_space = ["", "", "rooms_in_home_shared_facilities"]
+      expect(app.living_space).to eq(%w[rooms_in_home_shared_facilities])
     end
   end
 
