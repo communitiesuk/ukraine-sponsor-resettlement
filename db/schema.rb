0b8c86efa0ad1d80f3eb19661b7596a5a1e67278
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-04-13 16:52:51 +0100

    Amended schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_14_181901) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_13_151523) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_14_181901) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["reference"], name: "index_individual_expressions_of_interest_on_reference", unique: true
+  end
+
+  create_table "matches", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "reference"
+    t.json "answers"
+    t.datetime "transferred_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "organisation_expressions_of_interest", force: :cascade do |t|
