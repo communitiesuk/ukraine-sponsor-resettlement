e1edd4da9da00ab56090820543cc9864537abcd5
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-04-14 10:52:56 +0100

    Fix merge conflict

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,5 +4,38 @@ class IndividualExpressionOfInterest < ApplicationRecord
   self.table_name = "matches"
 
   SCHEMA_VERSION = 2
-  
+
+  attr_accessor :email, :fullname
+
+  after_initialize :after_initialize
+  before_save :serialize
+
+  after_find do
+    assign_attributes(answers)
+  end
+
+  def after_initialize
+    @final_submission = false
+  end
+
+  def as_json
+    {
+        id:,
+        reference:,
+        created_at:,
+        type:,
+        version:,
+        email:,
+        fullname:,
+        started_at:,
+    }.compact
+  end
+
+  private
+
+  def serialize
+    self.type = "match"
+    self.version = SCHEMA_VERSION
+    self.answers = as_json
+  end
 end
