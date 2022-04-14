43001d79282d3c4b2770a160105eab2cd207f10a
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-04-14 10:59:51 +0100

    Add schema after matches migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_13_151523) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_14_095035) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_13_151523) do
 
   create_table "matches", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "reference"
+    t.string "fullname", limit: 128
+    t.string "email", limit: 128
     t.json "answers"
     t.datetime "transferred_at"
     t.datetime "created_at", null: false
