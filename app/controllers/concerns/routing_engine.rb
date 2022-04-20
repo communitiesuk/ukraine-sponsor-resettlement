f32ceb2fd8f0582fa0c12f646d1322931230799f
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-04-20 20:52:33 +0100

    Amend routing rules for pivot based on resident hosting

--- app/controllers/concerns/routing_engine.rb
+++ app/controllers/concerns/routing_engine.rb
@@ -1,5 +1,11 @@
 class RoutingEngine
-  def self.get_next_step(current_step)
-    current_step + 1
+  def self.get_next_step(application, current_step)
+    if application.residential_host == "Yes" && current_step == 7
+      999
+    elsif application.residential_host == "No" && current_step == 5
+      8
+    else
+      current_step + 1
+    end
   end
 end
--- spec/routing_engine_spec.rb
+++ spec/routing_engine_spec.rb
@@ -3,10 +3,25 @@ require "rails_helper"
 RSpec.describe RoutingEngine, type: :model do
   describe "getting next step without routing" do
     it "when current to next step is incremental" do
-      expect(RoutingEngine.get_next_step(1)).to be(2)
-      expect(RoutingEngine.get_next_step(2)).to be(3)
-      expect(RoutingEngine.get_next_step(3)).to be(4)
-      expect(RoutingEngine.get_next_step(4)).to be(5)
+      application = AdditionalInfo.new
+
+      expect(RoutingEngine.get_next_step(application, 1)).to be(2)
+      expect(RoutingEngine.get_next_step(application,2)).to be(3)
+      expect(RoutingEngine.get_next_step(application,3)).to be(4)
+      expect(RoutingEngine.get_next_step(application,4)).to be(5)
+    end
+
+    it "when next step is dependent on application data" do
+      application = AdditionalInfo.new
+
+      application.residential_host = "Yes"
+      expect(RoutingEngine.get_next_step(application, 5)).to be(6)
+      application.residential_host = "Yes"
+      expect(RoutingEngine.get_next_step(application, 6)).to be(7)
+      application.residential_host = "Yes"
+      expect(RoutingEngine.get_next_step(application, 7)).to be(999)
+      application.residential_host = "No"
+      expect(RoutingEngine.get_next_step(application, 5)).to be(8)
     end
   end
 end
