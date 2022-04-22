005c9690ff04745801f5bb4124b31e82b14febd7
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-04-22 12:10:23 +0100

    Fix Linting issues

--- app/controllers/concerns/routing_engine.rb
+++ app/controllers/concerns/routing_engine.rb
@@ -1,10 +1,10 @@
 class RoutingEngine
   def self.get_next_step(application, current_step)
-    if application.residential_host.present? && application.residential_host.upcase == "NO" && current_step == 5
+    if application.residential_host.present? && application.residential_host.casecmp("NO").zero? && current_step == 5
       7
-    elsif application.residential_host.present? && application.residential_host.upcase == "YES" && current_step == 6
+    elsif application.residential_host.present? && application.residential_host.casecmp("YES").zero? && current_step == 6
       11
-    elsif application.more_properties.present? && application.more_properties.upcase == "NO" && current_step == 9
+    elsif application.more_properties.present? && application.more_properties.casecmp("NO").zero? && current_step == 9
       11
     else
       current_step + 1
--- spec/routing_engine_spec.rb
+++ spec/routing_engine_spec.rb
@@ -5,30 +5,30 @@ RSpec.describe RoutingEngine, type: :model do
     it "when current to next step is incremental" do
       application = AdditionalInfo.new
 
-      expect(RoutingEngine.get_next_step(application,1)).to be(2)
-      expect(RoutingEngine.get_next_step(application,2)).to be(3)
-      expect(RoutingEngine.get_next_step(application,3)).to be(4)
-      expect(RoutingEngine.get_next_step(application,4)).to be(5)
+      expect(described_class.get_next_step(application,1)).to be(2)
+      expect(described_class.get_next_step(application,2)).to be(3)
+      expect(described_class.get_next_step(application,3)).to be(4)
+      expect(described_class.get_next_step(application,4)).to be(5)
     end
 
     it "when next step is dependent on host question" do
       application = AdditionalInfo.new
 
       application.residential_host = "Yes"
-      expect(RoutingEngine.get_next_step(application, 5)).to be(6)
+      expect(described_class.get_next_step(application, 5)).to be(6)
       application.residential_host = "Yes"
-      expect(RoutingEngine.get_next_step(application, 6)).to be(11)
+      expect(described_class.get_next_step(application, 6)).to be(11)
       application.residential_host = "No"
-      expect(RoutingEngine.get_next_step(application, 5)).to be(7)
+      expect(described_class.get_next_step(application, 5)).to be(7)
     end
 
     it "when next step is dependent on more properties question" do
       application = AdditionalInfo.new
 
       application.more_properties = "Yes"
-      expect(RoutingEngine.get_next_step(application, 9)).to be(10)
+      expect(described_class.get_next_step(application, 9)).to be(10)
       application.more_properties = "No"
-      expect(RoutingEngine.get_next_step(application, 9)).to be(11)
+      expect(described_class.get_next_step(application, 9)).to be(11)
     end
   end
 end
