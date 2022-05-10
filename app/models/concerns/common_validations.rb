b9976d72bfbd020e1d2f8630c17b15a1edcdedb7
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-05-10 10:35:01 +0100

    Amend phone number validation now it is not optional

--- app/models/concerns/common_validations.rb
+++ app/models/concerns/common_validations.rb
@@ -48,10 +48,7 @@ private
   end
 
   def validate_phone_number
-    if @phone_number.present? &&
-        (@phone_number.scan(/\d/).join.length < MIN_PHONE_DIGITS ||
-            @phone_number.scan(/\d/).join.length > MAX_PHONE_DIGITS ||
-            !@phone_number.match(/[0-9 -+]+$/))
+    if @phone_number.nil? || @phone_number.scan(/\d/).join.length < 11 || @phone_number.scan(/\d/).join.length > 14 || !@phone_number.match(/[0-9 -+]+$/)
       errors.add(:phone_number, I18n.t(:invalid_phone_number, scope: :error))
     end
   end
