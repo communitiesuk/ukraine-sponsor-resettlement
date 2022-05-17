8fafc0cb522a87fe07ba2951a68cfce0fba270a0
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-05-17 15:52:47 +0100

    Amend question validation

--- spec/models/additional_info_spec.rb
+++ spec/models/additional_info_spec.rb
@@ -15,10 +15,25 @@ RSpec.describe AdditionalInfo, type: :model do
     it "validates the number of adults at a property is at least 0 when not residential property" do
       app = described_class.new
       app.different_address = "yes"
+      app.number_adults = ""
+      expect(app.valid?).to be(false)
+      expect(app.errors[:number_adults]).to include("Please enter a valid number of adults")
       app.number_adults = -1
       expect(app.valid?).to be(false)
       expect(app.errors[:number_adults]).to include("Please enter a valid number of adults")
       app.number_adults = 0
+      expect(app.valid?).to be(true)
+    end
+
+    it "validates the number of children at a property is at least 0" do
+      app = described_class.new
+      app.number_children = ""
+      expect(app.valid?).to be(false)
+      expect(app.errors[:number_children]).to include("Please enter a valid number of children")
+      app.number_children = -1
+      expect(app.valid?).to be(false)
+      expect(app.errors[:number_children]).to include("Please enter a valid number of children")
+      app.number_children = 0
       expect(app.valid?).to be(true)
     end
   end
