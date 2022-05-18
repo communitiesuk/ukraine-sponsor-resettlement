7872d2a525f36637af19e3dfbfdde8849e083cc4
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-05-18 08:11:36 +0100

    Remove commented out validation

--- app/models/additional_info.rb
+++ app/models/additional_info.rb
@@ -34,7 +34,6 @@ class AdditionalInfo < ApplicationRecord
   validate :validate_different_address, if: -> { run_validation? :different_address }
   validate :validate_user_research, if: -> { run_validation? :user_research }
   validate :validate_number_adults, if: -> { run_validation? :number_adults }
-  # validate :validate_number_children, if: -> { run_validation? :number_children }
   validates :number_children, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: I18n.t(:number_children, scope: :error) }, if: -> { run_validation? :number_children }
   validate :validate_allow_pet_pet, if: -> { run_validation? :allow_pet }
   validate :validate_more_properties, if: -> { run_validation? :more_properties }
