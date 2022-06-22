1b1c696c0650c2e039605ddffbf54262c0a7de23
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-06-22 09:09:05 +0100

    Add validation for minor date of birth

--- app/models/concerns/uam_validations.rb
+++ app/models/concerns/uam_validations.rb
@@ -5,10 +5,25 @@ module UamValidations
   MAX_ENTRY_DIGITS    = 128
 
   included do
+    validate :validate_minor_full_name, if: -> { run_validation? :minor_fullname }
+    validate :validate_minor_date_of_birth, if: -> { run_validation? :minor_date_of_birth }
     validate :validate_parent_consent_file_type, if: -> { run_validation? :parental_consent_file_type }
     validate :validate_parent_consent_filename, if: -> { run_validation? :parental_consent_filename }
-    validate :validate_minor_full_name, if: -> { run_validation? :minor_fullname }
     validate :validate_full_name, if: -> { run_validation? :fullname }
+  end
+
+  def validate_minor_full_name
+    if @minor_fullname.nil? || @minor_fullname.strip.length < MIN_ENTRY_DIGITS || @minor_fullname.strip.length > MAX_ENTRY_DIGITS || @minor_fullname.split.length < 2 || @minor_fullname.match(/[!"£$%{}<>|&@\/()=?^;]/)
+      errors.add(:minor_fullname, I18n.t(:invalid_minor_fullname, scope: :error))
+    end
+  end
+
+  def validate_minor_date_of_birth
+    if @minor_date_of_birth.nil? || @minor_date_of_birth.empty? || DateTime.parse(@minor_date_of_birth.map { |_, v| v }.join("-").to_s) >= DateTime.now.to_date
+      errors.add(:minor_date_of_birth, I18n.t(:invalid_minor_date_of_birth, scope: :error))
+    elsif DateTime.parse(@minor_date_of_birth.map { |_, v| v }.join("-").to_s) < 18.years.ago.to_date
+      errors.add(:minor_date_of_birth, I18n.t(:too_old_minor_date_of_birth, scope: :error))
+    end
   end
 
   def validate_parent_consent_file_type
@@ -23,11 +38,7 @@ module UamValidations
     end
   end
 
-  def validate_minor_full_name
-    if @minor_fullname.nil? || @minor_fullname.strip.length < MIN_ENTRY_DIGITS || @minor_fullname.strip.length > MAX_ENTRY_DIGITS || @minor_fullname.split.length < 2 || @minor_fullname.match(/[!"£$%{}<>|&@\/()=?^;]/)
-      errors.add(:minor_fullname, I18n.t(:invalid_minor_fullname, scope: :error))
-    end
-  end
+
 
   def validate_full_name
     if @fullname.nil? || @fullname.strip.length < MIN_ENTRY_DIGITS || @fullname.strip.length > MAX_ENTRY_DIGITS || @fullname.split.length < 2 || @fullname.match(/[!"£$%{}<>|&@\/()=?^;]/)
--- spec/models/unaccompanied_minor_spec.rb
+++ spec/models/unaccompanied_minor_spec.rb
@@ -1,6 +1,26 @@
 require "rails_helper"
 
 RSpec.describe UnaccompaniedMinor, type: :model do
+  describe "age validations" do
+    it "minor is less than 18", :focus do
+      app = described_class.new
+      app.minor_date_of_birth = {}
+      expect(app.valid?).to be(false)
+      expect(app.errors[:minor_date_of_birth]).to include("Enter a valid child's date of birth")
+      expect(app.errors[:minor_date_of_birth].count).to be(1)
+      app.minor_date_of_birth = { "1" => 2001, "2" => 6, "3" => 1 } #not ideal, but will 'work'
+      expect(app.valid?).to be(false)
+      expect(app.errors[:minor_date_of_birth]).to include("A person with this date of birth is over 18 and you cannot use this service to apply to sponsor them")
+      expect(app.errors[:minor_date_of_birth].count).to be(1)
+      app.minor_date_of_birth = { "1" => DateTime.now.year, "2" => DateTime.now.month, "3" => DateTime.now.day }
+      expect(app.valid?).to be(false)
+      expect(app.errors[:minor_date_of_birth]).to include("Enter a valid child's date of birth")
+      expect(app.errors[:minor_date_of_birth].count).to be(1)
+      app.minor_date_of_birth = { "1" => 2022, "2" => 6, "3" => 21 } #not ideal, but will 'work' for about 18 years
+      expect(app.valid?).to be(true)
+    end
+  end
+
   describe "validations for file upload" do
     it "ensure content type" do
       app = described_class.new
