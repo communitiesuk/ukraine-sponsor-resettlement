975804d236230c33624f8e2322588bc019568382
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-06-22 12:29:24 +0100

    Fix Linting issues

--- spec/system/unaccompanied_minor_spec.rb
+++ spec/system/unaccompanied_minor_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Unaccompanied minor expression of interest", type: :system do
   end
 
   describe "submitting the form" do
-    it "without parental consent form terminates early", :focus do
+    it "without parental consent form terminates early" do
       visit "/unaccompanied-minor"
       expect(page).to have_content("Apply for certification to sponsor a child travelling on their own")
 
@@ -27,7 +27,7 @@ RSpec.describe "Unaccompanied minor expression of interest", type: :system do
       expect(page).to have_content("You cannot apply without completed parental consent forms")
     end
 
-    it "saves all of the answers in the database", :focus do
+    it "saves all of the answers in the database" do
       visit "/unaccompanied-minor"
       expect(page).to have_content("Apply for certification to sponsor a child travelling on their own")
 
