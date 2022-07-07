0d35baef4f0d588ec4da27a500f3c12682361596
Samuele Mattiuzzo <samuele.mattiuzzo@madetech.com>
2022-07-07 10:53:18 +0100

    Allows dev server to serve static assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Serve static files
+  config.public_file_server.enabled = true
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
