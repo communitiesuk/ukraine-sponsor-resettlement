48c4ac7f6f5483b517f0ceb8ecd1a800c4d56456
Jack Sutton <jack.sutton@madetech.com>
2022-07-12 16:13:05 +0100

    linting

--- app/controllers/fraud_controller.rb
+++ app/controllers/fraud_controller.rb
@@ -1,24 +1,24 @@
 class FraudController < ApplicationController
-    add_flash_types :error
+  add_flash_types :error
 
-    def display
-      @abstractconfirm = AbstractConfirm.new 
-      render "fraud/fraud_page"
-    end
+  def display
+    @abstractconfirm = AbstractConfirm.new
+    render "fraud/fraud_page"
+  end
 
-    def post
-      @abstractconfirm = AbstractConfirm.new 
-      @abstractconfirm.assign_attributes(confirm_params)
-     if @abstractconfirm.valid?
-      ##if they confirm they will be redirected to external file 
+  def post
+    @abstractconfirm = AbstractConfirm.new
+    @abstractconfirm.assign_attributes(confirm_params)
+    if @abstractconfirm.valid?
+      # if they confirm they will be redirected to external file
       redirect_to "/"
-     else
-      #if they do not confirm reload page and show error 
+    else
+      # if they do not confirm reload page and show error
       render "fraud/fraud_page"
     end
   end
-  
-  private
+
+private
 
   def confirm_params
     params.require(:abstract_confirm).permit(:confirm_and_continue)
--- app/models/abstract_confirm.rb
+++ app/models/abstract_confirm.rb
@@ -1,8 +1,7 @@
-class AbstractConfirm 
-    include ActiveModel::Model
+class AbstractConfirm
+  include ActiveModel::Model
 
-    attr_accessor :confirm_and_continue
+  attr_accessor :confirm_and_continue
 
-    validates :confirm_and_continue, acceptance: { message: 'You must confirm to continue'}
-
-  end
+  validates :confirm_and_continue, acceptance: { message: "You must confirm to continue" }
+end
