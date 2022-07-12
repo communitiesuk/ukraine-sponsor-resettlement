6adbfa37822129351e7b0fb6a5b67616be53bac0
Jack Sutton <jack.sutton@madetech.com>
2022-07-12 13:44:10 +0100

    Added error message when checkbox is empty

---
+++ app/models/abstract_confirm.rb
@@ -0,0 +1,8 @@
+class AbstractConfirm 
+    include ActiveModel::Model
+
+    attr_accessor :confirm_and_continue
+
+    validates :confirm_and_continue, acceptance: { message: 'You must confirm to continue'}
+
+  end
