6741bc5d23e37846bf929f65e6f2e77d82c9ab29
Jack Sutton <jack.sutton@madetech.com>
2022-07-14 09:44:26 +0100

    back to seperate controller

--- app/controllers/sendapp_controller.rb
+++ app/controllers/sendapp_controller.rb
@@ -1,21 +1,25 @@
 class SendappController < ApplicationController
   add_flash_types :error
+  def display
+    @privacyconfirm = AbstractConfirm.new
+    render "/send-application/privacy"
+  end 
 
   def post
-    @privacyconfirm = PrivacyConfirm.new
+    @privacyconfirm = AbstractConfirm.new
     @privacyconfirm.assign_attributes(confirm_params)
     if @privacyconfirm.valid?
       # if they confirm they will be redirected to next page
-      redirect_to "/"
+      render "send-application/eligibility"
     else
       # if they do not confirm reload page and show error
-      render "/send-application/data_sharing"
+      render "/send-application/privacy"
     end
   end
 
 private
 
   def confirm_params
-    params.require(:privacy_confirm).permit(:privacy_statement_confirm)
+    params.require(:abstract_confirm).permit(:privacy_statement_confirm)
   end
 end
--- app/models/abstract_confirm.rb
+++ app/models/abstract_confirm.rb
@@ -1,7 +1,8 @@
 class AbstractConfirm
   include ActiveModel::Model
 
-  attr_accessor :confirm_and_continue
+  attr_accessor :confirm_and_continue, :privacy_statement_confirm
 
   validates :confirm_and_continue, acceptance: { message: "You must confirm to continue" }
+  validates :privacy_statement_confirm, acceptance: { message: "You must read and agree to the privacy statement to continue" }
 end
