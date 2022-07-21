8e8edf93f462e73a0c8eb1209b8d7dbe89500240
Jack Sutton <jack.sutton@madetech.com>
2022-07-21 10:37:43 +0100

    add test for blank date of birth entered

--- spec/system/unaccompanied_minor_spec.rb
+++ spec/system/unaccompanied_minor_spec.rb
@@ -500,4 +500,35 @@ RSpec.describe "Unaccompanied minor expression of interest", type: :system do
       expect(page).to have_content("We've sent you an email with a link to your saved application")
     end
   end
+  
+  describe "Goes through child flow and enters DoB", :focus do
+    it "enters blank DoB" do
+      new_application = UnaccompaniedMinor.new
+      new_application.save!
+
+      page_url = "/sponsor-a-child/task-list/#{new_application.reference}"
+
+      visit page_url
+      expect(page).to have_content("Apply for permission to sponsor a child fleeing Ukraine without a parent")
+
+      click_link("Child's personal details")
+      expect(page).to have_content("Enter the name of the child you want to sponsor")
+
+      fill_in("Given name(s)", with: "Jane")
+      fill_in("Family name", with: "Doe")
+
+      click_button("Continue")
+      expect(page).to have_content("How can we contact")
+
+      check("Email")
+      fill_in("Email", with: "unaccompanied.minor@test.com")
+
+      click_button("Continue")
+      expect(page).to have_content("date of birth")
+
+      click_button("Continue")
+      expect(page).to have_content("Enter a valid date of birth")
+
+    end
+  end
 end
