6d5f439385912fff5f33efae72c7f5a8812febc5
Samuele Mattiuzzo <samuele.mattiuzzo@madetech.com>
2022-07-27 11:38:53 +0100

    Send email + create application token + resume form now take into account the uuid

--- app/controllers/token_based_resume_controller.rb
+++ app/controllers/token_based_resume_controller.rb
@@ -3,22 +3,22 @@ class TokenBasedResumeController < ApplicationController
   ## skip_after_action :update_session_last_seen
 
   def session_expired
-    ##  send_email
+    send_email
     ##  session_reset
     render "token-based-resume/session_expired"
   end
 
   def display
-    @abstractresumetoken = AbstractResumeToken.new
+    @abstractresumetoken = AbstractResumeToken.new(magic_link: params[:uuid])
     render "token-based-resume/session_resume_form"
   end
 
   def submit
-    @abstractresumetoken = AbstractResumeToken.new
+    @abstractresumetoken = AbstractResumeToken.new(magic_link: params[:uuid])
     @abstractresumetoken.assign_attributes(confirm_params)
 
     if @abstractresumetoken.valid?
-      @applicationtoken = ApplicationToken.find_by(token: @abstractresumetoken.token)
+      @applicationtoken = ApplicationToken.find_by(magic_link: @abstractresumetoken.magic_link, token: @abstractresumetoken.token)
 
       if @applicationtoken.present?
         # the application exists, store in the session and let them resume
@@ -30,7 +30,7 @@ class TokenBasedResumeController < ApplicationController
       else
         # no application was found with this token
         flash[:error] = "No application found for this code"
-        redirect_to "/sponsor-a-child/resume-application"
+        redirect_to "/sponsor-a-child/resume-application?uuid=#{params[:uuid]}"
       end
     else
       # the token is invalid
@@ -41,20 +41,32 @@ class TokenBasedResumeController < ApplicationController
 private
 
   def confirm_params
-    params.require(:abstract_resume_token).permit(:token)
+    params.require(:abstract_resume_token).permit(:token, :magic_link)
   end
 
-  # #def send_email
-  ##  @application = UnaccompaniedMinor.find_by_reference(session[:app_reference])
-  ##  return_link = url_for(:action => 'display', :controller => 'token_based_resume_controller', :only_path => false)
-  ##  GovNotifyMailer.send_save_and_return_email(@application.email, @application.given_name, return_link).deliver_later
-  # #end
-  ##
-  # #def create_and_send_code
-  ##  @application = UnaccompaniedMinor.find_by_reference(session[:app_reference])
-  ##  generate_code = 6.times.map{rand(10)}.join
-  ##  ApplicationToken.new(unaccompanied_minor: application, token: generate_code)
-  ##
-  ##
-  # #end
+  def generate_magic_link(uuid)
+    base_url = url_for(action: "display", controller: "token_based_resume", only_path: false)
+    "#{base_url}?uuid=#{uuid}"
+  end
+
+  def generate_token
+    Array.new(6) { rand(10) }.join
+  end
+
+  def generate_application_token(application, magic_link)
+    application_token = ApplicationToken.new(
+      unaccompanied_minor: application,
+      magic_link:,
+      token: generate_token,
+    )
+    application_token.save!
+  end
+
+  def send_email
+    @application = UnaccompaniedMinor.find_by_reference(session[:app_reference])
+    personal_uuid = SecureRandom.uuid
+    return_link = generate_magic_link(personal_uuid)
+    generate_application_token(@application, personal_uuid)
+    GovNotifyMailer.send_save_and_return_email(@application.email, @application.given_name, return_link).deliver_later
+  end
 end
--- app/models/abstract_resume_token.rb
+++ app/models/abstract_resume_token.rb
@@ -1,7 +1,7 @@
 class AbstractResumeToken
   include ActiveModel::Model
 
-  attr_accessor :token
+  attr_accessor :token, :magic_link
 
   validates :token, presence: { message: "The code is required" },
                     numericality: { message: "You must insert a valid 6-digit code" }
