514a824cbbf81482db995107be4491d499a85761
Neil Kidd <neil.kidd@madetech.com>
2022-07-29 13:25:41 +0100

    clean up spec

--- spec/controllers/token_based_resume_controller_spec.rb
+++ spec/controllers/token_based_resume_controller_spec.rb
@@ -2,8 +2,12 @@ require "rails_helper"
 
 RSpec.describe TokenBasedResumeController, type: :controller do
   describe "User tries to resume their application" do
+    phone_number = "07983111111".freeze
+    sms_code = "123456".freeze
+    magic_id = "e5c4fe58-a8ca-4e6f-aaa6-7e0a381eb3dc".freeze
+
     uam = UnaccompaniedMinor.new
-    uam.phone_number = "07983111111"
+    uam.phone_number = phone_number
 
     let(:texter) { instance_double("Notifications::Client") }
     let(:application_token) { instance_double("ApplicationToken") }
@@ -11,13 +15,13 @@ RSpec.describe TokenBasedResumeController, type: :controller do
     before do
       allow(Notifications::Client).to receive(:new).and_return(texter)
       allow(texter).to receive(:send_sms)
-      allow(ApplicationToken).to receive(:find_by).and_return(ApplicationToken.new({ token: "123456", unaccompanied_minor: uam, magic_link: "e5c4fe58-a8ca-4e6f-aaa6-7e0a381eb3dc" }))
+      allow(ApplicationToken).to receive(:find_by).and_return(ApplicationToken.new({ token: sms_code, unaccompanied_minor: uam, magic_link: magic_id }))
     end
 
     it "routes to send_token" do
-      get :display, params: { magic_link: "test" }
+      get :display, params: { magic_link: magic_id }
 
-      expect(texter).to have_received(:send_sms).with({ personalisation: { OTP: "123456" }, phone_number: "07983111111", template_id: "b51a151e-f352-473a-b52e-185d2873cbf5" })
+      expect(texter).to have_received(:send_sms).with({ personalisation: { OTP: sms_code }, phone_number:, template_id: "b51a151e-f352-473a-b52e-185d2873cbf5" })
     end
   end
 end
