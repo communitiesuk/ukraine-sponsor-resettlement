c7ffeaa25dbb87018ad3600dfcf57356ae009c45
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-08-04 12:54:27 +0100

    Return 405 from Production

--- app/controllers/s3_bucket_controller.rb
+++ app/controllers/s3_bucket_controller.rb
@@ -1,5 +1,9 @@
 class S3BucketController < ApplicationController
   def list_objects
+    if Rails.env.production?
+      render :nothing => true, :status => :method_not_allowed
+    end
+
     @objects = S3Object.new
 
     @service = StorageService.new(PaasConfigurationService.new, ENV["INSTANCE_NAME"])
