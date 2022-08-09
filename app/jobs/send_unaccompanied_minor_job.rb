9f67dfa66cd13e81ab8786a307f569895b0d00fd
Neil Kidd <neil.kidd@madetech.com>
2022-08-09 14:47:47 +0100

    rename env var with UAM prefix

--- app/jobs/send_unaccompanied_minor_job.rb
+++ app/jobs/send_unaccompanied_minor_job.rb
@@ -5,7 +5,7 @@ class SendUnaccompaniedMinorJob < ApplicationJob
     Rails.logger.info "Sending update for unaccompanied minor #{id}"
     TransferRecord.execute_unaccompanied_minor(id)
 
-    if ENV["FEATURE_TRANSFER_CONSENT_FORMS"] == "true"
+    if ENV["UAM_FEATURE_TRANSFER_CONSENT_FORMS"] == "true"
       TransferRecord.execute_unaccompanied_minor_consent_forms(id)
     end
   end
