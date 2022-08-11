bbd232a09fe13b90e01c437f27fcdf9e959e3fc6
Samuele Mattiuzzo <samuele.mattiuzzo@madetech.com>
2022-08-11 17:51:50 +0100

    Specs for save and return

--- spec/system/token_based_resume_controller_spec.rb
+++ spec/system/token_based_resume_controller_spec.rb
@@ -17,4 +17,47 @@ RSpec.describe TokenBasedResumeController, type: :system do
       expect(page).to have_content("Your session has timed out due to inactivity")
     end
   end
+
+  describe "User intentionally resumes their application" do
+    phone_number = "07983111111".freeze
+    email = "test@email.com".freeze
+    given_name = "First".freeze
+    family_name = "Given".freeze
+
+    sms_code = 123_456
+    magic_id = "e5c4fe58-a8ca-4e6f-aaa6-7e0a381eb3dc".freeze
+    expiry_time = (Time.zone.now.utc + 1.hour)
+
+    uam = UnaccompaniedMinor.new
+    uam.phone_number = phone_number
+    uam.email = email
+    uam.given_name = given_name
+    uam.family_name = family_name
+    uam.save!
+
+    let(:texter) { instance_double("Notifications::Client") }
+    let(:application_token) { instance_double("ApplicationToken") }
+    let(:task_list_content) { "Apply for permission to sponsor a child fleeing Ukraine without a parent".freeze }
+
+    before do
+      allow(Notifications::Client).to receive(:new).and_return(texter)
+      allow(texter).to receive(:send_sms)
+      allow(ApplicationToken).to receive(:find_by).and_return(ApplicationToken.new({ token: sms_code, unaccompanied_minor: uam, magic_link: magic_id, expires_at: expiry_time }))
+    end
+
+    it "shows the confirm page if all info are entered" do
+      page.set_rack_session(app_reference: uam.reference)
+      visit "/sponsor-a-child/save-and-return"
+
+      expect(page).to have_content("We've sent the link to the email address you have provided.")
+    end
+
+    it "loads correct application given code" do
+      params = { abstract_resume_token: { token: sms_code }, uuid: magic_id }
+
+      post "/sponsor-a-child/resume-application", params:
+
+      # expect(request.body).to have_content(task_list_content)
+    end
+  end
 end
