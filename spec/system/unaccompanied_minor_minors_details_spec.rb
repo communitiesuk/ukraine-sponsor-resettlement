a7f402c19abcd60e3720f29728960db0a1ed5e5f
Neil Kidd <neil.kidd@madetech.com>
2022-08-24 13:11:58 +0100

    test full happy path for minor personal details

--- spec/system/unaccompanied_minor_minors_details_spec.rb
+++ spec/system/unaccompanied_minor_minors_details_spec.rb
@@ -6,39 +6,41 @@ RSpec.describe "Unaccompanied minor minors details", type: :system do
     driven_by(:rack_test_user_agent)
   end
 
-  describe "submitting the form for child's flow" do
-    it "saves all the minors data" do
+  describe "completing the minors details with valid input" do
+    it "saves all the minors data and shows completed status" do
       new_application = UnaccompaniedMinor.new
       new_application.save!
-      minor_dob_under_18_year = Time.zone.now.year - 4
-
       page.set_rack_session(app_reference: new_application.reference)
 
+      minor_dob_under_18_year = Time.zone.now.year - 4
+      task_list_content = "Apply for permission to sponsor a child fleeing Ukraine without a parent".freeze
+
       visit "/sponsor-a-child/task-list"
-      expect(page).to have_content("Apply for permission to sponsor a child fleeing Ukraine without a parent")
+      expect(page).to have_content(task_list_content)
 
       click_link("Child's personal details")
       expect(page).to have_content("Enter the name of the child you want to sponsor")
 
-      fill_in("Given name(s)", with: "Jane")
-      fill_in("Family name", with: "Doe")
+      fill_in("Given name(s)", with: "Child")
+      fill_in("Family name", with: "Minor")
 
       click_button("Continue")
-      expect(page).to have_content("How can we contact")
+      expect(page).to have_content("How can we contact the child?")
 
       check("Email")
-      fill_in("Email", with: "unaccompanied.minor@test.com")
+      fill_in("Email", with: "minor@example.com")
 
       click_button("Continue")
-      expect(page).to have_content("Jane Doe")
+      expect(page).to have_content("Child Minor")
       expect(page).to have_content("Enter their date of birth")
 
-      fill_in("Day", with: 3)
-      fill_in("Month", with: 6)
+      fill_in("Day", with: 1)
+      fill_in("Month", with: 1)
       fill_in("Year", with: minor_dob_under_18_year)
 
       click_button("Continue")
-      expect(page).to have_content("Apply for permission to sponsor a child fleeing Ukraine without a parent")
+      expect(page).to have_content(task_list_content)
+      expect(page).to have_content("completed").once
     end
   end
 
