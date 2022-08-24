e4f21c2a4361422790fc4dda9eab2c95ad2b1865
Neil Kidd <neil.kidd@madetech.com>
2022-08-24 16:16:38 +0100

    Add basic tests around common email validator

--- spec/models/concerns/common_validations_spec.rb
+++ spec/models/concerns/common_validations_spec.rb
@@ -1,18 +1,40 @@
 RSpec.describe CommonValidations, type: :helper do
   describe "name validation" do
     it "returns false when name is not valid" do
-      invalid_names = [nil, "", " ", "!", "$", "£", "A" * 129].freeze
+      test_values = [nil, "", " ", "!", "$", "£", "A" * 129].freeze
 
-      invalid_names.each do |name|
-        expect(is_valid_name?(name)).to be(false), "Failed value:#{name.inspect}"
+      test_values.each do |val|
+        expect(is_valid_name?(val)).to be(false), "Failed value:#{val.inspect}"
       end
     end
 
     it "returns true when name is valid" do
-      valid_names = ["a", "AB", "apos'ok", "Spaces OK", "Hypens-OK"].freeze
+      test_values = ["a", "AB", "apos'ok", "Spaces OK", "Hypens-OK"].freeze
 
-      valid_names.each do |name|
-        expect(is_valid_name?(name)).to be(true), "Failed value:#{name.inspect}"
+      test_values.each do |val|
+        expect(is_valid_name?(val)).to be(true), "Failed value:#{val.inspect}"
+      end
+    end
+  end
+
+  describe "email validation" do
+    it "returns false when email is not valid" do
+      test_values = [nil, "", " ", "!", "$", "£", "A" * 129].freeze
+
+      test_values.each do |val|
+        expect(email_address_valid?(val)).to be(false), "Failed value:#{val.inspect}"
+      end
+    end
+
+    it "returns true when email is valid" do
+      test_values = ["a@b.it",
+                     "hello_world@a.it",
+                     "freddy+smith@gmail.com",
+                     "test.email-some+name@example.com",
+                     "me@a-very-long-domain.it"].freeze
+
+      test_values.each do |val|
+        expect(email_address_valid?(val)).to be(true), "Failed value:#{val.inspect}"
       end
     end
   end
