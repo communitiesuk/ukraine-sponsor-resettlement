4bbbe2c5a2115d6567c4c52a88a8ea5c50aaf825
Samuele Mattiuzzo <samuele.mattiuzzo@madetech.com>
2022-09-05 12:04:59 +0100

    Wrap migration in begin/rescue to identify dirty records

--- db/migrate/20220902121930_update_email_date_of_birth.rb
+++ db/migrate/20220902121930_update_email_date_of_birth.rb
@@ -1,46 +1,53 @@
 class UpdateEmailDateOfBirth < ActiveRecord::Migration[7.0]
   def change
     UnaccompaniedMinor.all.each do |uam|
-      # migrates the old date of birth from
-      # 3 separate values to _date_of_birth = {3: day, 2: month, 1: year}
-      #
-      # migrates the old email from
-      # a string to an array ["", "email"] or ["", "none"]
+      begin
+        # migrates the old date of birth from
+        # 3 separate values to _date_of_birth = {3: day, 2: month, 1: year}
+        #
+        # migrates the old email from
+        # a string to an array ["", "email"] or ["", "none"]
 
-      updated_answers = uam.answers.deep_dup
+        updated_answers = uam.answers.deep_dup
 
-      if uam.answers.key?("sponsor_date_of_birth_day")
-        sponsor_date_of_birth = {
-          "3" => uam.answers["sponsor_date_of_birth_day"],
-          "2" => uam.answers["sponsor_date_of_birth_month"],
-          "1" => uam.answers["sponsor_date_of_birth_year"],
-        }
-        updated_answers = updated_answers.except(
-          "sponsor_date_of_birth_day", "sponsor_date_of_birth_month", "sponsor_date_of_birth_year"
-        )
-        updated_answers["sponsor_date_of_birth"] = sponsor_date_of_birth
+        if uam.answers.key?("sponsor_date_of_birth_day")
+          sponsor_date_of_birth = {
+            "3" => uam.answers["sponsor_date_of_birth_day"],
+            "2" => uam.answers["sponsor_date_of_birth_month"],
+            "1" => uam.answers["sponsor_date_of_birth_year"],
+          }
+          updated_answers = updated_answers.except(
+            "sponsor_date_of_birth_day", "sponsor_date_of_birth_month", "sponsor_date_of_birth_year"
+          )
+          updated_answers["sponsor_date_of_birth"] = sponsor_date_of_birth
+        end
+
+        if uam.answers.key?("minor_date_of_birth_day")
+          minor_date_of_birth = {
+            "3" => uam.answers["minor_date_of_birth_day"],
+            "2" => uam.answers["minor_date_of_birth_month"],
+            "1" => uam.answers["minor_date_of_birth_year"],
+          }
+          updated_answers = updated_answers.except(
+            "minor_date_of_birth_day", "minor_date_of_birth_month", "minor_date_of_birth_year"
+          )
+          updated_answers["minor_date_of_birth"] = minor_date_of_birth
+        end
+
+        if uam.answers.key?("minor_contact_type") && (uam.answers["minor_contact_type"].is_a? String)
+          updated_answers["minor_contact_type"] = ["", uam.answers["minor_contact_type"]]
+        end
+
+        if uam.answers != updated_answers
+          uam.assign_attributes(updated_answers)
+          uam.save!(validate: false)
+        end
       end
-
-      if uam.answers.key?("minor_date_of_birth_day")
-        minor_date_of_birth = {
-          "3" => uam.answers["minor_date_of_birth_day"],
-          "2" => uam.answers["minor_date_of_birth_month"],
-          "1" => uam.answers["minor_date_of_birth_year"],
-        }
-        updated_answers = updated_answers.except(
-          "minor_date_of_birth_day", "minor_date_of_birth_month", "minor_date_of_birth_year"
-        )
-        updated_answers["minor_date_of_birth"] = minor_date_of_birth
-      end
-
-      if uam.answers.key?("minor_contact_type") && (uam.answers["minor_contact_type"].is_a? String)
-        updated_answers["minor_contact_type"] = ["", uam.answers["minor_contact_type"]]
-      end
-
-      if uam.answers != updated_answers
-        uam.assign_attributes(updated_answers)
-        uam.save!(validate: false)
-      end
+    rescue StandardError => e
+      Rails.logger.error "****************************************************************"
+      Rails.logger.error "ERROR: #{e.message}"
+      Rails.logger.error "Failing record id: #{uam.id}"
+      Rails.logger.error "****************************************************************"
     end
   end
 end
