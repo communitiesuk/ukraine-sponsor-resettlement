04ad26c2275620cf550e63221bb643a3df1199d4
Neil Kidd <neil.kidd@madetech.com>
2022-09-27 10:13:48 +0100

    fix brakeman warning for risk of Dynamic render paths

--- app/controllers/unaccompanied_controller.rb
+++ app/controllers/unaccompanied_controller.rb
@@ -121,8 +121,9 @@ class UnaccompaniedController < ApplicationController
   end
 
   def handle_upload_uk
+    current_stage = params["stage"].to_i
     @application = UnaccompaniedMinor.find_by_reference(session[:app_reference])
-    @application.started_at = Time.zone.now.utc if params["stage"].to_i == 1
+    @application.started_at = Time.zone.now.utc if current_stage == 1
     @application.uk_parental_consent_filename = ""
 
     begin
@@ -140,13 +141,14 @@ class UnaccompaniedController < ApplicationController
     if @application.valid?
       save_and_redirect(@application.uk_parental_consent_saved_filename, upload_params.tempfile)
     else
-      render "sponsor-a-child/steps/#{params['stage']}"
+      render "sponsor-a-child/steps/#{current_stage}"
     end
   end
 
   def handle_upload_ukraine
+    current_stage = params["stage"].to_i
     @application = UnaccompaniedMinor.find_by_reference(session[:app_reference])
-    @application.started_at = Time.zone.now.utc if params["stage"].to_i == 1
+    @application.started_at = Time.zone.now.utc if current_stage == 1
     @application.ukraine_parental_consent_filename = ""
 
     begin
@@ -164,7 +166,7 @@ class UnaccompaniedController < ApplicationController
     if @application.valid?
       save_and_redirect(@application.ukraine_parental_consent_saved_filename, upload_params.tempfile)
     else
-      render "sponsor-a-child/steps/#{params['stage']}"
+      render "sponsor-a-child/steps/#{current_stage}"
     end
   end
 
@@ -699,7 +701,6 @@ private
 
   def render_current_step
     current_step = params["stage"].to_i
-    current_step.freeze
 
     render "sponsor-a-child/steps/#{current_step}"
   end
