17cb7cdbb5cac7899242c36713484969ee0d48f7
Neil Kidd <neil.kidd@madetech.com>
2022-09-29 13:10:37 +0100

    WIP - adds sponsors details to json schema

--- spec/models/unaccompanied_minor_json_adapter_spec.rb
+++ spec/models/unaccompanied_minor_json_adapter_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe UnaccompaniedMinor, type: :model do
       app.save!
 
       populate_eligibility_and_extras(app)
+      populate_sponsor_info(app)
 
-      json = JSON.generate(app.as_json)
+      json = JSON.pretty_generate(app.as_json)
 
       puts json
 
@@ -28,5 +29,30 @@ RSpec.describe UnaccompaniedMinor, type: :model do
     uam.is_committed = "yes"
     uam.have_parental_consent = "yes"
     uam.is_permitted = "yes"
+  end
+
+  def populate_sponsor_info(uam)
+    uam.given_name = "Firstname"
+    uam.family_name = "Familyname"
+    uam.phone_number = "07777 123 456"
+    uam.email = "sponsor@example.com"
+    uam.has_other_names = "false"
+    uam.nationality = "GBR - United Kingdom"
+    uam.sponsor_date_of_birth = {
+      3 => 1,
+      2 => 6,
+      1 => Time.zone.now.year - 36,
+    }
+    uam.has_other_nationalities = "false"
+    uam.residential_line_1 = "Address line 1"
+    uam.residential_line_2 = "Address line 2"
+    uam.residential_town = "Address town"
+    uam.residential_postcode = "BS2 0AX"
+    uam.other_adults_address = "no"
+    # DANGER: uam.different_address actually means the user answered "yes" when asked
+    # "Will you (the sponsor) be living at this address?"
+    # uam.different_address = "yes"
+    uam.identification_type = "passport"
+    uam.identification_number = "ABC123"
   end
 end
