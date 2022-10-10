613335b9ffadaa00178e160df46ee8fa55c59b77
Jack Sutton <jack.sutton@madetech.com>
2022-10-10 12:28:31 +0100

    adds correct error message to locales

--- spec/system/uam_sponsor_contact_details_spec.rb
+++ spec/system/uam_sponsor_contact_details_spec.rb
@@ -77,6 +77,30 @@ RSpec.describe "Sponsor contact details", type: :system do
       expect(page).to have_field("Confirm phone number", with: valid_phone_number)
     end
 
+    it "validates that the phone number is UK" do 
+      fill_in_email_and_continue
+      expect(page).to have_content(phone_page_content)
+      
+      fill_in_phone_numbers_and_continue(phone_number: "12312312312", phone_number_confirm: "12312312312")
+      expect(page).to have_content("Error: You must enter a UK mobile number")
+    end
+
+    it "recognises UK mobile number with 447 rather than 07" do 
+      fill_in_email_and_continue
+      expect(page).to have_content(phone_page_content)
+      
+      fill_in_phone_numbers_and_continue(phone_number: "447312312312", phone_number_confirm: "447312312312")
+      expect(page).to have_content(task_list_content)
+    end
+
+    it "recognises UK mobile number with +447 rather than 07" do 
+      fill_in_email_and_continue
+      expect(page).to have_content(phone_page_content)
+      
+      fill_in_phone_numbers_and_continue(phone_number: "+447312312312", phone_number_confirm: "+447312312312")
+      expect(page).to have_content(task_list_content)
+    end
+
     def fill_in_phone_numbers_and_continue(phone_number: valid_phone_number, phone_number_confirm: valid_phone_number)
       fill_in("unaccompanied-minor-phone-number-field", with: phone_number)
       fill_in("Confirm phone number", with: phone_number_confirm)
