9d03e53a529373355fbbb5e3bebd5b24a43b3923
JEREMY LORD <jlord@zaizi.com>
2022-10-25 14:49:42 +0100

    start page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,10 @@
 class IndexController < ApplicationController
   def index
     @feature_eoi_journey_enabled = (ENV["FEATURE_EOI_JOURNEY_ENABLED"] == "true")
+    if ENV["FEATURE_EOI_JOURNEY_ENABLED"] == "true"
+      render "index/index_eoi"
+    else
+      render "index/index"
+    end
   end
 end
