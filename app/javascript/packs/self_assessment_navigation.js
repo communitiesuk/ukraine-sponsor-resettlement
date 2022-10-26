b26a6bb68265ea9aaf3c00e200d0cb4f9ddc0a15
marcus petty-saphon <marcusps1@gmail.com>
2022-10-26 17:04:58 +0100

    adding validation for self assessment

--- app/javascript/packs/self_assessment_navigation.js
+++ app/javascript/packs/self_assessment_navigation.js
@@ -1,21 +1,37 @@
 (function() {
 
-  var selfAssesmentContinueButton = document.getElementById('selfAssessmentContinueButton');
-    if(selfAssesmentContinueButton) {
-        selfAssesmentContinueButton.addEventListener('click', function() {
-            var elements = document.getElementsByName('assessment-buttons');
-            var selectedOption;
-        
-            for (var i=0, len=elements.length; i<len; ++i) {
-                if (elements[i].checked){
-                    selectedOption = elements[i].dataset.next;
-                    break;
-                }
-            }
-            if(selectedOption) {
-                window.location.href = selectedOption;
-            }
-          })
-    }
-
-} )()
+    var selfAssesmentContinueButton = document.getElementById('selfAssessmentContinueButton');
+    var formGroup = document.querySelector('.govuk-form-group');
+    var errorMessage = document.querySelector('.govuk-error-message')
+  
+  
+      if(selfAssesmentContinueButton) {
+  
+          selfAssesmentContinueButton.addEventListener('click', function() {
+              var elements = document.getElementsByName('assessment-buttons');
+              var selectedOption;
+  
+              if(!formGroup) {
+                  return false
+              }
+          
+              for (var i=0, len=elements.length; i<len; ++i) {
+                  if (elements[i].checked){
+                      selectedOption = elements[i].dataset.next;
+                      break;
+                  }
+              }
+              if(selectedOption) {
+                  window.location.href = selectedOption;
+                  formGroup.classList.remove('govuk-form-group--error')
+                  errorMessage.style.display = 'none';
+              
+              } else {
+                  formGroup.classList.add('govuk-form-group--error')
+                  errorMessage.style.display = 'block';
+              }
+            
+            })
+      }
+  
+  } )()
