677db3bf0b496a7280c55abe50eaa9064b50de0a
Samuele Mattiuzzo <samuele.mattiuzzo@madetech.com>
2022-11-21 17:21:55 +0000

    Remove exposed url. Cypress in its own workflow

--- automated_tests/cypress.config.js
+++ automated_tests/cypress.config.js
@@ -18,9 +18,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
       require('cypress-high-resolution')(on, config)
-      
+
     },
-    baseUrl: 'https://ukraine:r3fug3@ukraine-sponsor-resettlement-staging.london.cloudapps.digital/',
+    baseUrl: 'http://localhost:8080',
   },
 });
 
