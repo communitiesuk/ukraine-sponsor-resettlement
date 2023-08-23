3aba999a63fe3dbee048a59e5aa670983f830532
Samuele Mattiuzzo <samuele.mattiuzzo@madetech.com>
2023-08-23 15:36:39 +0100

    Desperate attempt to increase Redis timeout for AWS/Elasticache/Sidekiq

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -5,7 +5,13 @@ if Rails.env.development? || Rails.env.test?
   Rack::Attack.enabled = false
 else
   redis_url = PaasConfigurationService.new.redis_uris[:"ukraine-sponsor-resettlement-#{Rails.env}-redis"]
-  Rack::Attack.cache.store = ActiveSupport::Cache::RedisCacheStore.new(url: redis_url)
+  Rack::Attack.cache.store = ActiveSupport::Cache::RedisCacheStore.new(
+    url: redis_url,
+    connect_timeout: 60,  # Defaults to 20 seconds
+    read_timeout: 5, # Defaults to 1 second
+    write_timeout: 5, # Defaults to 1 second
+    reconnect_attempts: 1,   # Defaults to 0
+  )
 end
 
 Rack::Attack.throttle("requests by ip", limit: 60, period: 60.seconds) do |request|
