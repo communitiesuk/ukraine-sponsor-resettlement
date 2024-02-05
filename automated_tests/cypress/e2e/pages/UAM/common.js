53b3c65378a78fdd5ff6ac3ec9808a7efd9c3118
aamircodes <aamirp1@outlook.com>
2024-02-05 14:21:22 +0000

    Updated content sponsor-a-child homepage, steps 2,4

--- automated_tests/cypress/e2e/pages/UAM/common.js
+++ automated_tests/cypress/e2e/pages/UAM/common.js
@@ -3,9 +3,9 @@ const elements = require('../../page_elements/UAM/uam_elements')
 export const uam_start_header = () => { cy.get(elements.main_heading).contains("Apply to provide a safe home for a child from Ukraine").should('be.visible').wait(Cypress.env('waitTime'))}
 export const uam_check_header = () => { cy.get(elements.main_heading).contains('Check if you are eligible to use this service').should('be.visible')}
 export const uam_step1_header = () => { cy.get(elements.page_heading).contains('Is the person you want to sponsor under 18?').should('be.visible')}
-export const uam_step2_header = () => { cy.get(elements.page_heading).contains('Was the child living in Ukraine on or before 31 December 2021?').should('be.visible')}
+export const uam_step2_header = () => { cy.get(elements.page_heading).contains('Was the child living in Ukraine before 1 January 2022?').should('be.visible')}
 export const uam_step3_header = () => { cy.get(elements.page_heading).contains('Was the child born after 31 December 2021?').should('be.visible') }
-export const uam_step4_header = () => { cy.get(elements.page_heading).contains('Are they travelling to the UK with a parent or legal guardian?').should('be.visible')}
+export const uam_step4_header = () => { cy.get(elements.page_heading).contains('Are they applying for a visa under the Homes for Ukraine Scheme with their parent or legal guardian, or to join them in the UK?').should('be.visible')}
 export const uam_step5_header = () => { cy.get(elements.page_heading).contains('Can you upload both consent forms?').should('be.visible')}
 export const uam_step6_header = () => { cy.get(elements.page_heading).contains('Can you commit to hosting the child for the minimum period?').should('be.visible')}
 export const uam_step7_header = () => { cy.get(elements.page_heading).contains('Do you have permission to live in the UK for the minimum period?').should('be.visible')}
--- spec/support/unaccompanied_minor_helpers.rb
+++ spec/support/unaccompanied_minor_helpers.rb
@@ -16,10 +16,10 @@ module UnaccompaniedMinorHelpers
     expect(page).to have_content("Is the person you want to sponsor under 18?")
     uam_choose_option("Yes")
 
-    expect(page).to have_content("Was the child living in Ukraine on or before 31 December 2021?")
+    expect(page).to have_content("Was the child living in Ukraine before 1 January 2022?")
     uam_choose_option("Yes")
 
-    expect(page).to have_content("Are they travelling to the UK with a parent or legal guardian?")
+    expect(page).to have_content("Are they applying for a visa under the Homes for Ukraine Scheme with their parent or legal guardian, or to join them in the UK?")
     uam_choose_option("No")
 
     expect(page).to have_content("Can you upload both consent forms?")
