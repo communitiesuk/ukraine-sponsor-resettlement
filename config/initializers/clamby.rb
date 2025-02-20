89c378b98a9ef752822d40b9741d389a59198bba
Alice Lee <alice.lee@digital.cabinet-office.gov.uk>
2025-02-20 15:09:20 +0000

    Default to using local clamd config file

--- config/initializers/clamby.rb
+++ config/initializers/clamby.rb
@@ -1,6 +1,6 @@
 host = ENV.fetch("CLAMD_HOST", "localhost")
 port = ENV.fetch("CLAMD_PORT", "3310")
-config_path = Rails.env.development? || Rails.env.test? ? "config/clamd.conf" : "/etc/clamav/clamd.conf"
+config_path = ENV.fetch("CLAMD_CONFIG_PATH", "config/clamd.conf")
 
 config = %{
 TCPAddr #{host}
